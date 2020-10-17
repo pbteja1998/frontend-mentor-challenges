8096d824757a56f75ab663d23b1512522242c0e3
Bhanu Teja P <pbteja1998@gmail.com>
2020-10-18 04:02:19 +0530

    Add github-jobs-api project

--- script.js
+++ script.js
@@ -1,75 +1,82 @@
 const projects = [
   {
-    name: "fylo-data-storage-component",
+    name: 'fylo-data-storage-component',
   },
   {
-    name: "four-card-feature-section",
+    name: 'four-card-feature-section',
   },
   {
-    name: "coding-bootcamp-testimonials-slider",
+    name: 'coding-bootcamp-testimonials-slider',
   },
   {
-    name: "base-apparel-coming-soon",
+    name: 'base-apparel-coming-soon',
   },
   {
-    name: "article-preview-component",
+    name: 'article-preview-component',
   },
   {
-    name: "intro-component-with-signup-form",
+    name: 'intro-component-with-signup-form',
   },
   {
-    name: "single-price-grid-component",
+    name: 'single-price-grid-component',
   },
   {
-    name: "ping-coming-soon-page",
+    name: 'ping-coming-soon-page',
   },
   {
-    name: "huddle-landing-page-with-alternating-feature-blocks",
+    name: 'huddle-landing-page-with-alternating-feature-blocks',
   },
   {
-    name: "huddle-landing-page-with-single-introductory-section",
+    name: 'huddle-landing-page-with-single-introductory-section',
   },
   {
-    name: "fylo-landing-page-with-two-column-layout",
+    name: 'fylo-landing-page-with-two-column-layout',
   },
   {
-    name: "social-media-dashboard-with-theme-switcher",
+    name: 'social-media-dashboard-with-theme-switcher',
   },
   {
-    name: "insure-landing-page",
+    name: 'insure-landing-page',
   },
   {
-    name: "pricing-component-with-toggle",
+    name: 'pricing-component-with-toggle',
   },
   {
-    name: "project-tracking-intro-component",
+    name: 'project-tracking-intro-component',
   },
   {
-    name: "job-listings-with-filtering",
+    name: 'job-listings-with-filtering',
     external: true,
     imageSrc:
-      "https://raw.githubusercontent.com/pbteja1998/jobs-board/master/frontendmentor/design/desktop-design.jpg",
-    previewUrl: "https://jobs-board.bhanuteja.dev/",
-    githubSrc: "https://github.com/pbteja1998/jobs-board",
+      'https://raw.githubusercontent.com/pbteja1998/jobs-board/master/frontendmentor/design/desktop-design.jpg',
+    previewUrl: 'https://jobs-board.bhanuteja.dev/',
+    githubSrc: 'https://github.com/pbteja1998/jobs-board',
   },
-];
+  {
+    name: 'github-jobs-api',
+    external: true,
+    imageSrc: 'https://jobs.bhanuteja.dev/preview.jpg',
+    previewUrl: 'https://jobs.bhanuteja.dev/',
+    githubSrc: 'https://github.com/pbteja1998/github-jobs-api',
+  },
+]
 
-const list = document.getElementById("list");
+const list = document.getElementById('list')
 
 projects.forEach(({ name, external, imageSrc, previewUrl, githubSrc }, i) => {
-  const listItem = document.createElement("li");
-  let imgLink;
-  let previewLink;
-  let githubLink;
+  const listItem = document.createElement('li')
+  let imgLink
+  let previewLink
+  let githubLink
 
   if (external) {
-    imgLink = imageSrc;
-    previewLink = previewUrl;
-    githubLink = githubSrc;
+    imgLink = imageSrc
+    previewLink = previewUrl
+    githubLink = githubSrc
   } else {
-    imgLink = `${name}/design/desktop-design.jpg`;
-    previewLink = `${name}/index.html`;
-    githubLink = `https://github.com/pbteja1998/frontend-mentor-challenges/tree/master/${name}`;
+    imgLink = `${name}/design/desktop-design.jpg`
+    previewLink = `${name}/index.html`
+    githubLink = `https://github.com/pbteja1998/frontend-mentor-challenges/tree/master/${name}`
   }
 
   listItem.innerHTML = `
@@ -85,21 +92,21 @@ projects.forEach(({ name, external, imageSrc, previewUrl, githubSrc }, i) => {
         <a href="${githubLink}" class="blue">
           <i class="fas fa-code"></i>
         </a>
-      </div>`;
-  list.appendChild(listItem);
-});
+      </div>`
+  list.appendChild(listItem)
+})
 
-const emptyListItem = document.createElement("li");
-list.appendChild(emptyListItem);
-list.appendChild(emptyListItem);
-list.appendChild(emptyListItem);
-list.appendChild(emptyListItem);
-list.appendChild(emptyListItem);
-list.appendChild(emptyListItem);
+const emptyListItem = document.createElement('li')
+list.appendChild(emptyListItem)
+list.appendChild(emptyListItem)
+list.appendChild(emptyListItem)
+list.appendChild(emptyListItem)
+list.appendChild(emptyListItem)
+list.appendChild(emptyListItem)
 
 function formatProjectName(name) {
   return name
-    .split("-")
+    .split('-')
     .map((word) => word[0].toUpperCase() + word.slice(1))
-    .join(" ");
+    .join(' ')
 }
